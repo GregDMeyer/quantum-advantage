5ddfd025e8a61aab01347050e96fa32daf97ed9a
Greg Meyer <gregory.meyer@gmail.com>
2021-03-25 15:49:24 -0700

    fix optimize_cutoff

--- analysis/optimize_cutoff.py
+++ analysis/optimize_cutoff.py
@@ -1,6 +1,7 @@
 
 import random
-from sys import argv
+from argparse import ArgumentParser
+from itertools import chain
 
 import cirq
 
@@ -9,35 +10,33 @@ import sys
 from os.path import abspath, dirname, join
 sys.path.append(join(dirname(abspath(__file__)), '../circuits'))
 
-from count_gates import describe
+from count_gates import describe, fast_flatten
 from ancilla import AncillaManager
 from digital_circuits import karatsuba_mult, schoolbook_mult
 from digital_circuits import karatsuba_square, schoolbook_square
 from digital_circuits import karatsuba_classical_mult, schoolbook_classical_mult
 
-def compare_counts(cutoff, karatsuba, schoolbook):
-    ck = cirq.Circuit()
-    cs = cirq.Circuit()
 
-    karatsuba(ck, cutoff)
-    schoolbook(cs, cutoff)
+def parse_args():
+    p = ArgumentParser(
+        description='Optimize the cutoff for Karatsuba recursion.'
+    )
+    p.add_argument('op', choices=['mult', 'square', 'classical'],
+                   default='mult', help='which operation to optimize')
+    return p.parse_args()
 
-    k_gates, _ = describe(ck)
-    s_gates, _ = describe(cs)
-
-    return k_gates, s_gates
 
 def main():
     cutoff = 3
 
-    if argv[1] == 'mult':
+    args = parse_args()
+
+    if args.op == 'mult':
         karatsuba, schoolbook = get_mult_fns()
-    elif argv[1] == 'square':
+    elif args.op == 'square':
         karatsuba, schoolbook = get_square_fns()
-    elif argv[1] == 'classical':
+    elif args.op == 'classical':
         karatsuba, schoolbook = get_classical_mult_fns()
-    else:
-        raise ValueError(f"unknown mult type '{argv[1]}'")
 
     k_count = 1
     s_count = 0
@@ -47,7 +46,19 @@ def main():
     while k_count >= s_count:
         cutoff += 1
         k_count, s_count = compare_counts(cutoff, karatsuba, schoolbook)
-        print(f"{str(cutoff).rjust(6)} {str(k_count).rjust(10)} {str(s_count).rjust(10)}")
+        print(f"{str(cutoff).rjust(6)} {str(k_count).rjust(10)} "
+              f"{str(s_count).rjust(10)}")
+
+
+def compare_counts(cutoff, karatsuba, schoolbook):
+    ck = karatsuba(cutoff)
+    cs = schoolbook(cutoff)
+
+    k_gates, _, _ = describe(fast_flatten(ck))
+    s_gates, _, _ = describe(fast_flatten(cs))
+
+    return k_gates, s_gates
+
 
 def get_mult_fns():
     def get_registers(cutoff):
@@ -58,13 +69,14 @@ def get_mult_fns():
         ancillas = AncillaManager()
         return a_reg, b_reg, c_reg, ancillas
 
-    def karatsuba(circ, cutoff):
-        karatsuba_mult(circ, *get_registers(cutoff), cutoff=cutoff)
+    def karatsuba(cutoff):
+        return karatsuba_mult(*get_registers(cutoff), cutoff=cutoff)
 
-    def schoolbook(circ, cutoff):
-        schoolbook_mult(circ, *get_registers(cutoff))
+    def schoolbook(cutoff):
+        return schoolbook_mult(*get_registers(cutoff))
 
     return karatsuba, schoolbook
+
 
 def get_square_fns():
     def get_registers(cutoff):
@@ -74,13 +86,14 @@ def get_square_fns():
         ancillas = AncillaManager()
         return a_reg, c_reg, ancillas
 
-    def karatsuba(circ, cutoff):
-        karatsuba_square(circ, *get_registers(cutoff), cutoff=cutoff)
+    def karatsuba(cutoff):
+        return karatsuba_square(*get_registers(cutoff), cutoff=cutoff)
 
-    def schoolbook(circ, cutoff):
-        schoolbook_square(circ, *get_registers(cutoff))
+    def schoolbook(cutoff):
+        return schoolbook_square(*get_registers(cutoff))
 
     return karatsuba, schoolbook
+
 
 def get_classical_mult_fns():
 
@@ -98,15 +111,29 @@ def get_classical_mult_fns():
         ancillas = AncillaManager()
         return a, b_reg, c_reg, ancillas
 
-    def karatsuba(circ, cutoff):
+    def karatsuba(cutoff):
+        rtn = []
         for it in range(iters):
-            karatsuba_classical_mult(circ, *get_args(cutoff, it), cutoff=cutoff)
+            rtn.append(
+                karatsuba_classical_mult(
+                    *get_args(cutoff, it),
+                    cutoff=cutoff
+                )
+            )
+        return chain(*rtn)
 
-    def schoolbook(circ, cutoff):
+    def schoolbook(cutoff):
+        rtn = []
         for it in range(iters):
-            schoolbook_classical_mult(circ, *get_args(cutoff, it))
+            rtn.append(
+                karatsuba_classical_mult(
+                    *get_args(cutoff, it),
+                )
+            )
+        return chain(*rtn)
 
     return karatsuba, schoolbook
+
 
 if __name__ == '__main__':
     main()
